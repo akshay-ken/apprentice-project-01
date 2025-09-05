b60746937757ce0a8d7f6aa39e497812bb3f1715
akshay-ken <akshaybest3120@gmail.com>
2025-09-05 19:26:12 +0530

    style: use img tag and padding to header

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,6 +1,8 @@
+import logo from "../assets/images/logo.svg";
 export function HeaderSection() {
   return (
-    <header>
+    <header className="px-4 md:px-6 py-6 md:py-8">
+      <img src={logo} alt="" />
       <nav></nav>
     </header>
   );

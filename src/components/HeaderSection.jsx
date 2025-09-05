b6f57040302380ee71ba9274cd2396f3d01bc3ed
akshay-ken <akshaybest3120@gmail.com>
2025-09-05 19:31:40 +0530

    style: add menu open img and hide it on desktop view

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,9 +1,12 @@
 import logo from "../assets/images/logo.svg";
+import menuOpen from "../assets/images/icon-menu.svg";
 export function HeaderSection() {
   return (
     <header className="px-4 md:px-6 py-6 md:py-8">
       <img src={logo} alt="" />
-      <nav></nav>
+      <nav>
+        <img src={menuOpen} className="block md:hidden" alt="" />
+      </nav>
     </header>
   );
 }

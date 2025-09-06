23dc8fc94f8bdd1e5dda21aeef4b9f3a43ca07a4
akshay-ken <akshaybest3120@gmail.com>
2025-09-06 13:59:36 +0530

    style: add styling to navbar text

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -5,14 +5,14 @@ export function HeaderSection() {
     <header className="px-4 md:px-6 py-6 md:py-8 flex flex-row justify-between md:justify-start items-center">
       <img src={logo} alt="" />
       <img src={menuOpen} className="block md:hidden" alt="" />
-      <nav className="hidden md:flex flex-row ml-20 justify-between w-full">
-        <ul className="flex flex-row gap-x-10">
+      <nav className="hidden md:flex flex-row ml-20 justify-between w-full text-Gray-500 font-medium text-base">
+        <ul className="flex flex-row gap-x-10 ">
           <li>Features</li>
           <li>Company</li>
           <li>Careers</li>
           <li>About</li>
         </ul>
-        <div className="flex flex-row gap-x-10">
+        <div className="flex flex-row gap-x-10 font-normal text-base">
           <button>Login</button>
           <button>Register</button>
         </div>

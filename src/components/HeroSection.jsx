0af1d3507d228c7f81c42baf876bf0133f56527c
akshay-ken <akshaybest3120@gmail.com>
2025-09-06 14:20:57 +0530

    style: add footer for mobile view

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,16 @@
 import heroMobile from "../assets/images/image-hero-mobile.png";
-import heroDeskto from "../assets/images/image-hero-desktop.png";
+import heroDesktop from "../assets/images/image-hero-desktop.png";
+import clientOne from "../assets/images/client-audiophile.svg";
+import clientTwo from "../assets/images/client-databiz.svg";
+import clientThree from "../assets/images/client-maker.svg";
+import clientFour from "../assets/images/client-meet.svg";
 
 export function HeroSection() {
   return (
     <section className="md:px-[10vw] flex flex-col md:flex-row">
       <div>
         <img src={heroMobile} className="block md:hidden" alt="" />
+        <img src={heroDesktop} className="hidden md:block" alt="" />
       </div>
       <div className="flex flex-col justify-evenly mt-12 items-center gap-y-6">
         <h1 className="text-4xl font-bold text-Gray-950">Make remote work</h1>
@@ -17,6 +22,12 @@ export function HeroSection() {
           Learn more
         </button>
       </div>
+      <footer className="flex flex-row h-6 justify-around mt-14">
+        <img src={clientOne} alt="" />
+        <img src={clientTwo} alt="" />
+        <img src={clientThree} alt="" />
+        <img src={clientFour} alt="" />
+      </footer>
     </section>
   );
 }

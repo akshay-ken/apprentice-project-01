f5c48fe5b962be3c6433f766fd5c5a7d706194b0
akshay-ken <akshaybest3120@gmail.com>
2025-09-07 16:09:39 +0530

    feat: add initial state values to be used with reudcerhook

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,5 +1,14 @@
 import logo from "../assets/images/logo.svg";
 import menuOpen from "../assets/images/icon-menu.svg";
+
+const initialState = {
+  features: {
+    isOpen: false,
+  },
+  company: {
+    isOpen: false,
+  },
+};
 export function HeaderSection() {
   return (
     <header className="px-4 md:px-6 py-6 md:py-8 flex flex-row justify-between md:justify-start items-center">
